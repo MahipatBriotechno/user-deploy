f8c9c0f02c0e112399c1252bc5c208ea96647c2f
Mahipat.Briotechno <102782100+MahipatBriotechno@users.noreply.github.com>
2022-07-07 15:07:24 +0530

    update window location

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,13 @@
 import React from "react"
 import { Match } from '@reach/router'
 
-
+const isBrowser = () => typeof window !== "undefined"
 export default function Home() {
   <>
     <Match path="/">
       {
-        window.location.href="/User/Pages/homepage"
+        isBrowser() && window.location.replace("/User/Pages/homepage")
+       
       }
     </Match>
   </>
